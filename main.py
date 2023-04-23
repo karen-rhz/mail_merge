118ac42245926f67e59e0049e7be9ade186cce8b
karen-rhz <127526793+karen-rhz@users.noreply.github.com>
2023-04-22 23:24:49 -0400

    Fetching the names and replace it to the header

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ letter = open("./Input/Letters/starting_letter.txt")
 body_letter = letter.readlines()
 # Working with only the header.
 headline_letter = body_letter[0]
+
+# Replace the [name] placeholder with the actual name.
+name = open("./Input/Names/invited_names.txt")
+names = []
+with open("./Input/Names/invited_names.txt") as list_of_names:
+    for line in list_of_names:
+        # .readline gets only 1 line from the .txt
+        # .strip removes spaces from strings
+        get_name = name.readline().strip()
+        names.append(get_name)
+
+final_headers = []
+for person in names:
+    write_name = headline_letter.replace("[name]", person)
+    final_headers.append(write_name)
