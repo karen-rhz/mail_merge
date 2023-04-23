f18ce36d399d922a62674380afbe4bd319397b97
karen-rhz <127526793+karen-rhz@users.noreply.github.com>
2023-04-22 23:23:45 -0400

    Divide the doc into manageable pieces

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # TODO: Create a letter using starting_letter.txt
 
 # for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+letter = open("./Input/Letters/starting_letter.txt")
+# .readlines returns a text in a list of strings
+body_letter = letter.readlines()
+# Working with only the header.
+headline_letter = body_letter[0]
